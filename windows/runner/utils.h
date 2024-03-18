0409bdff6312568eed3cad500d347eefd7b4851b
Jihyuk Lee <lead@sver.dev>
2024-03-19 01:44:02 +0900

    ➕ :: 홈 페이지 일부 요소 퍼블리싱
    아직 완성은 X, 진행중

--- windows/runner/utils.h
+++ windows/runner/utils.h
@@ -8,7 +8,7 @@
 // it for both the runner and the Flutter library.
 void CreateAndAttachConsole();
 
-// Takes a null-terminated wchar_t* encoded in UTF-16 and returns a std::string
+// Takes a home_page.dart-terminated wchar_t* encoded in UTF-16 and returns a std::string
 // encoded in UTF-8. Returns an empty std::string on failure.
 std::string Utf8FromUtf16(const wchar_t* utf16_string);
 
